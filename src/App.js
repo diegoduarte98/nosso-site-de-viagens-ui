d0ac283d7e4b998d9837f9a3649c833e12a5fdbc
Diego Duarte <diegoduarte98@gmail.com>
2020-03-29 23:59:41 -0300

    plugando o axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Pais from './components/pais/'
 import Aeroporto from './components/aeroporto/'
 import Rota from './components/rota/'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
